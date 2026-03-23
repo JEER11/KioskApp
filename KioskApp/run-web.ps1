8bf0edf879026c3af15b6bb4aded8219cf7eaaf6
Jeraldine <jeraldinerb@gmail.com>
2026-03-23 10:36:09 -0400

    Commit all changes

--- KioskApp/run-web.ps1
+++
@@ -1,9 +0,0 @@
-# KioskApp RunWeb PowerShell Script
-# Installs dependencies, builds components, and starts the map-template dev server
-
-npm install
-cd packages/components
-npm install
-npm run build
-cd ../map-template
-npx vite
